17b01667f12338f6a7cb3c44d8bced12c06da64f
lelopez28 <lelopez@poderjudicial.gob.do>
2025-03-12 05:44:41 -0600

    derecho2.py

--- Juegos de Litigios/derecho2.py
+++ Juegos de Litigios/derecho2.py
@@ -708,7 +708,7 @@ if __name__ == "__main__":
     if os.getenv("FLASK_ENV") != "production":
         port = int(os.getenv("PORT", 5000))
         app.run(host="0.0.0.0", port=port)
-        else:
+    else:
         import time
         print("Manteniendo el contenedor activo...")
         while True:
